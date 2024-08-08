38acde68c11fb3a91da5a3ef1d311716efce2860
shameemmonir <shameemmonir@gmail.com>
2024-08-08 07:54:53 +0600

    get base and height done

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -1,3 +1,23 @@
+/**
+ * Objective: get base, height of a triangle. Calculate the area by using the provided formula. and then display the area.
+ * step-01: get base value
+ * */ 
+
+
+
+
 function calculateTriangleArea(){
-    console.log('Inside function');
+    const triangleBaseInput = document.getElementById('triangle-base');
+    // console.log(triangleBaseInput);
+    const triangleBaseText = triangleBaseInput.value;
+    // console.log(typeof triangleBaseText);
+    const base = parseFloat(triangleBaseText);
+    console.log(base);
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    // console.log(triangleHeightInput);
+    const triangleHeightText = triangleHeightInput.value;
+    // console.log(triangleHeightText);
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
 }
